ced7d796ca091f3d99bbb7ae13d1a28e6f525931
Christophe Gueret <christophe.gueret@gmail.com>
2011-02-17 18:57:12 +0100

    Update

--- src/Interface.py
+++ src/Interface.py
@@ -4,6 +4,7 @@ Created on Feb 17, 2011
 @author: cgueret
 '''
 import gtk
+import gobject
 from sugar.graphics import style
 #image_table.attach(self.image, 0, 2, 0, 1, xoptions=gtk.FILL | gtk.SHRINK, yoptions=gtk.FILL | gtk.SHRINK, xpadding=10, ypadding=10)
 
@@ -19,14 +20,37 @@ class SortingPanel(object):
         self.image_ebox.drag_source_set(gtk.gdk.BUTTON1_MASK, [], gtk.gdk.ACTION_MOVE)
         self.image_ebox.drag_source_add_image_targets()
 
+        # Create the notebook for the boxes
+        self.panels = gtk.Notebook()
+        self.panels.set_scrollable(True)
         
         # Create the main widget and pack all the elements
         self.widget = gtk.HBox()
         left_part = gtk.VBox()
         left_part.add(self.image_ebox)
-        self.widget.add(left_part)
-        right_part = gtk.Notebook()
-        self.widget.add(right_part)
+        self.widget.pack_start(left_part, False, False, style.DEFAULT_SPACING)
+        self.widget.pack_end(self.panels, True, True, style.DEFAULT_SPACING)
+    
+    def add_box(self, name):
+        '''
+        Add a new box
+        '''
+        model = gtk.ListStore(gobject.TYPE_STRING, gtk.gdk.Pixbuf)
+        box_content = gtk.IconView(model)
+        box_content.set_text_column(0)
+        box_content.set_pixbuf_column(1)
+        #box_content.set_reorderable(False)
+        box_content.drag_dest_set(gtk.DEST_DEFAULT_ALL, [], gtk.gdk.ACTION_MOVE)
+        box_content.drag_dest_add_image_targets()
+        box_content.connect("drag_drop", self.image_dropped_cb)
+        scrollable = gtk.ScrolledWindow()
+        scrollable.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        scrollable.add(box_content)
+        hbox = gtk.HBox()
+        hbox.pack_start(gtk.image_new_from_stock(gtk.STOCK_DIRECTORY, 32), False, False, 10)
+        hbox.pack_end(gtk.Label(name), True, True, 10)
+        hbox.show_all()
+        self.panels.append_page(scrollable, hbox)
         
     def get_widget(self):
         '''
@@ -43,8 +67,17 @@ class SortingPanel(object):
         scaled_buffer = gtk.gdk.pixbuf_new_from_file_at_size(filename, style.zoom(160), style.zoom(120))
         self.image.set_from_pixbuf(scaled_buffer)
         self.image_ebox.drag_source_set_icon_pixbuf(scaled_buffer)
-        
-        
+    
+    def image_dropped_cb(self, widget, drag_context, x, y, time):
+        '''
+        @type drag_context gtk.gdk.DragContext
+        '''
+        print widget, drag_context, x, y, time
+        print drag_context.get_source_widget()
+        a = gtk.gdk.pixbuf_new_from_file_at_size("rubberDuck.jpg", style.zoom(160), style.zoom(120))
+        widget.get_model().append(["fdfdfd", a])
+        pass
+    
 class MainWindow(object):
     def __init__(self):
         '''
@@ -60,6 +93,11 @@ class MainWindow(object):
         
         # Create the panels of the application
         sortingPanel = SortingPanel()
+        sortingPanel.add_box("Box 1")
+        sortingPanel.add_box("Box 2")
+        sortingPanel.add_box("Box 3")
+        sortingPanel.add_box("Box 4")
+        sortingPanel.add_box("Box 5")
         
         # Pack everything
         self.panels = gtk.Notebook()
