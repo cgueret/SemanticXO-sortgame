bedde31fde2e942aefa65eb96fc4ca851908467e
Christophe Gueret <christophe.gueret@gmail.com>
2011-02-18 15:08:00 +0100

    The drag and drop works! :)

--- src/Interface.py
+++ src/Interface.py
@@ -8,75 +8,108 @@ import gobject
 from sugar.graphics import style
 #image_table.attach(self.image, 0, 2, 0, 1, xoptions=gtk.FILL | gtk.SHRINK, yoptions=gtk.FILL | gtk.SHRINK, xpadding=10, ypadding=10)
 
+DROP_TREE_MOVE = [
+                  ("GTK_TREE_MODEL_ROW", gtk.TARGET_SAME_APP, 0),
+                  ("image/png", 0, 1),
+                  ("STRING", 0, 2),
+                  ("text/plain", 0, 3)
+                ]
+
 class SortingPanel(object):
+    TARGET = [("text/plain", gtk.TARGET_SAME_APP, 0)]
+    images = {}
+    
     def __init__(self):
         '''
         Constructor
         '''
-        # Create the image to move and wrap it into an event box
-        self.image = gtk.Image()
-        self.image_ebox = gtk.EventBox()
-        self.image_ebox.add(self.image)
-        self.image_ebox.drag_source_set(gtk.gdk.BUTTON1_MASK, [], gtk.gdk.ACTION_MOVE)
-        self.image_ebox.drag_source_add_image_targets()
+        # Create the left part
+        left_part = gtk.Notebook()
+        left_part.set_show_tabs(False)
+        model = gtk.ListStore(gobject.TYPE_STRING, gtk.gdk.Pixbuf)
+        self.mess = gtk.IconView(model)
+        self.mess.set_text_column(0)
+        self.mess.set_pixbuf_column(1)
+        #self.mess.drag_source_set(gtk.gdk.BUTTON1_MASK, [], gtk.gdk.ACTION_MOVE)
+        self.mess.enable_model_drag_source(gtk.gdk.BUTTON1_MASK, self.TARGET , gtk.gdk.ACTION_MOVE)
+        self.mess.enable_model_drag_dest(self.TARGET, gtk.gdk.ACTION_MOVE)
+        self.mess.connect("drag_data_get", self.drag_cb)
+        self.mess.connect("drag_data_received", self.drop_cb)
+        scrollable = gtk.ScrolledWindow()
+        scrollable.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        scrollable.add(self.mess)
+        left_part.append_page(scrollable, gtk.Label("Things to sort"))
 
-        # Create the notebook for the boxes
-        self.panels = gtk.Notebook()
-        self.panels.set_scrollable(True)
+        # Create the notebook for the boxes (right part)
+        self.boxes = gtk.Notebook()
+        self.boxes.set_scrollable(True)
         
         # Create the main widget and pack all the elements
         self.widget = gtk.HBox()
-        left_part = gtk.VBox()
-        left_part.add(self.image_ebox)
-        self.widget.pack_start(left_part, False, False, style.DEFAULT_SPACING)
-        self.widget.pack_end(self.panels, True, True, style.DEFAULT_SPACING)
+        self.widget.pack_start(left_part, True , True, style.DEFAULT_SPACING)
+        self.widget.pack_end(self.boxes, True, True, style.DEFAULT_SPACING)
+    
+    def add_item(self, file_name):
+        '''
+        Change the image that shall be moved into a box
+        '''
+        name = file_name.split('.')[0]
+        if name not in self.images.keys():
+            self.images[name] = gtk.gdk.pixbuf_new_from_file_at_size(file_name, style.zoom(160), style.zoom(120))
+        self.mess.get_model().append([name, self.images[name]])
     
     def add_box(self, name):
         '''
-        Add a new box
+        Add a new box to the panels on the right
         '''
         model = gtk.ListStore(gobject.TYPE_STRING, gtk.gdk.Pixbuf)
-        box_content = gtk.IconView(model)
-        box_content.set_text_column(0)
-        box_content.set_pixbuf_column(1)
-        #box_content.set_reorderable(False)
-        box_content.drag_dest_set(gtk.DEST_DEFAULT_ALL, [], gtk.gdk.ACTION_MOVE)
-        box_content.drag_dest_add_image_targets()
-        box_content.connect("drag_drop", self.image_dropped_cb)
+        box = gtk.IconView(model)
+        box.set_text_column(0)
+        box.set_pixbuf_column(1)
+        box.set_reorderable(True)
+        box.enable_model_drag_source(gtk.gdk.BUTTON1_MASK, self.TARGET, gtk.gdk.ACTION_MOVE)
+        box.enable_model_drag_dest(self.TARGET, gtk.gdk.ACTION_MOVE)
+        box.drag_dest_add_image_targets()
+        box.connect("drag_data_get", self.drag_cb)
+        box.connect("drag_data_received", self.drop_cb)
         scrollable = gtk.ScrolledWindow()
         scrollable.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
-        scrollable.add(box_content)
+        scrollable.add(box)
+        scrollable.show_all()
         hbox = gtk.HBox()
-        hbox.pack_start(gtk.image_new_from_stock(gtk.STOCK_DIRECTORY, 32), False, False, 10)
+        hbox.pack_start(gtk.image_new_from_stock(gtk.STOCK_DIRECTORY, gtk.ICON_SIZE_BUTTON), False, False, 10)
         hbox.pack_end(gtk.Label(name), True, True, 10)
         hbox.show_all()
-        self.panels.append_page(scrollable, hbox)
+        self.boxes.append_page(scrollable, hbox)
+        
+    def drag_cb(self, iconview, context, selection, target_id, etime):
+        print 'get'
+        model = iconview.get_model()
+        iter = model.get_iter(iconview.get_selected_items()[0])
+        selection.set_text(model.get_value(iter, 0))
+        model.remove(iter)
+
+    def drop_cb(self, iconview, context, x, y, selection, info, etime):
+        print 'receive'
+        model = iconview.get_model()
+        name = selection.get_text()
+        model.append([name, self.images[name]])
         
     def get_widget(self):
         '''
         Return the widget of this panel
         '''
         self.widget.show_all()
-        self.set_image("rubberDuck.jpg")
         return self.widget
-
-    def set_image(self, filename):
-        '''
-        Change the image that shall be moved into a box
-        '''
-        scaled_buffer = gtk.gdk.pixbuf_new_from_file_at_size(filename, style.zoom(160), style.zoom(120))
-        self.image.set_from_pixbuf(scaled_buffer)
-        self.image_ebox.drag_source_set_icon_pixbuf(scaled_buffer)
     
-    def image_dropped_cb(self, widget, drag_context, x, y, time):
+    def item_dropped_cb(self, widget, drag_context, x, y, time):
         '''
-        @type drag_context gtk.gdk.DragContext
+        Called when an item is dropped somewhere
         '''
         print widget, drag_context, x, y, time
         print drag_context.get_source_widget()
         a = gtk.gdk.pixbuf_new_from_file_at_size("rubberDuck.jpg", style.zoom(160), style.zoom(120))
         widget.get_model().append(["fdfdfd", a])
-        pass
     
 class MainWindow(object):
     def __init__(self):
@@ -93,11 +126,6 @@ class MainWindow(object):
         
         # Create the panels of the application
         sortingPanel = SortingPanel()
-        sortingPanel.add_box("Box 1")
-        sortingPanel.add_box("Box 2")
-        sortingPanel.add_box("Box 3")
-        sortingPanel.add_box("Box 4")
-        sortingPanel.add_box("Box 5")
         
         # Pack everything
         self.panels = gtk.Notebook()
@@ -106,7 +134,12 @@ class MainWindow(object):
         self.panels.append_page(sortingPanel.get_widget(), gtk.Label("Sort items"))
         self.window.add(self.panels)
         self.window.show_all()
-        
+
+        # Add some content to the sort application        
+        sortingPanel.add_box("Box 1")
+        sortingPanel.add_box("Box 2")
+        sortingPanel.add_item("rubberDuck.jpg")
+        sortingPanel.add_item("chair.jpg")
 
     def keypress_cb(self, widget, event) :
         if event.keyval == gtk.keysyms.Escape or event.keyval == gtk.keysyms.Return :
@@ -121,3 +154,4 @@ if __name__ == '__main__':
     gtk.main()
 
 
+# http://www.kksou.com/php-gtk2/articles/drag-and-drop-between-two-thumbnail-images-using-GtkIconView-with-GtkTreeModelFilter---Part-1---left-to-right-append.php
